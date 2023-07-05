ac43b52c2824559ddde5eae625d823ec4e9149cc
Atakan Zengin <atakanzzengin@gmail.com>
2023-07-05 23:16:51 +0200

    Add gitignore, and change initial view with progress bar

---
+++ Scrumdinger/MeetingView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by Atakan Zengin on 05/07/2023.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        ProgressView(value: 5, total: 15)
+    }
+}
+
+#Preview {
+    MeetingView()
+}
